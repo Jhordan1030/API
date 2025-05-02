cca38918c68ee1a75d66cf52eb9afb2566b9d388
JhordanH <jhordanhuera2002@gmail.com>
2025-05-02 18:28:02 -0500

    verificacion de lanzamineot en azure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/hola', (req, res) => {
     res.send('¡Hola desde la API!');
 });
 
